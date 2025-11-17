fc8f7a5a26a449e5cbc58e74c588a0fc2190e01f
Benjamin Istace <bistace@ad-cng.cng.fr>
2025-11-17 12:01:02 +0000

    Increase filesystem latency max wait time to 70 seconds

--- hobrac/main.py
+++ hobrac/main.py
@@ -81,7 +81,7 @@ def link_busco_dir(src_dir: str, dest_dir: str):
 
 
 def get_base_snakemake_args(args) -> str:
-    cmd = "snakemake --latency-wait 30 --jobs 100 -p --skip-script-cleanup "
+    cmd = "snakemake --latency-wait 70 --jobs 100 -p --skip-script-cleanup "
 
     # Fallback to greedy scheduler if CBC solver is not available to avoid PuLP errors
     if shutil.which("cbc") is None:
